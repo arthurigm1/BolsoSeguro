d72aab4b3e9a0059a119dfdc447528e19e4642be
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-03-07 15:13:14 -0300

    ajustando tamanhos de imagens

--- src/app/TelaInicial/header/header.component.ts
+++ src/app/TelaInicial/header/header.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
